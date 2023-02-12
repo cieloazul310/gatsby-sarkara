333e742dbee17cd9f3d52ad82b4cd0e342007bb7
cieloazul310 <cieloazul310@gmail.com>
2023-02-12 10:08:51 +0900

    refactor(layout): add title to components

--- apps/vite/src/App.tsx
+++ apps/vite/src/App.tsx
@@ -1,10 +1,5 @@
 import * as React from 'react';
-import {
-  BasicLayout,
-  Paper,
-  H2,
-  Paragraph,
-} from '@cieloazul310/sarkara';
+import { BasicLayout, Paper, H2, Paragraph } from '@cieloazul310/sarkara';
 
 function App() {
   return (
